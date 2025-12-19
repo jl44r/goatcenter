3db8e057e3e27e084c4e8ce31528b7c141f4da37
jl44r <abode20031290@gmail.com>
2025-12-19 19:49:54 +0300

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,19 @@
+// js/firebase-config.js
+// Firebase v9 (modular) initialization (CDN module imports)
+// Replace the values in firebaseConfig with the config from your Firebase console.
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+// TODO: replace these placeholders with your Firebase project config
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
